d7d4de294b0c4b9c0c901b84e96a8fd6fdadcc0d
Lyric Farzamfar <lyricfarzamfar@Lyrics-MacBook-Pro.local>
2023-12-13 23:52:25 +0000

    complete code

--- cutoff_hand_sum.py
+++ cutoff_hand_sum.py
@@ -32,12 +32,12 @@ def hand_sum_calc(cards):
             total += card_val(card)
     if aces > 0:
         if ((total + aces - 1 + 11) <= 21):
-            total =  total + aces - 1 + 11
+            total =  int(total + aces - 1 + 11)
         else:
-            total = (total + aces)
-    if (total == 21 and len(cards == 2)):
+            total = int(total + aces)
+    if (total == 21 and len(cards) == 2):
         return -1
-    return total
+    return int(total)
 
 
 def handsum_upcard_game(start_hand, upcard, cutoff):
@@ -127,19 +127,118 @@ def cutoff_avs_for_upcard(upcard_val):
     cutoff_avs = []
     for cutoff in cutoffs:
         cutoff_tot = 0
-        for i in range(1):
+        for i in range(20):
             cutoff_tot += average_for_upcard_for_cutoff(upcard_val, cutoff)
-        cutoff_avs.append(cutoff_tot / 1)
-    print(cutoff_avs)
+        cutoff_avs.append(cutoff_tot / 20)
     return cutoff_avs
-cutoff_res = [cutoff_avs_for_upcard(i) for i in range(1, 14)]
-colormap = cm.get_cmap('tab20', 13)
-colors = [colormap(i) for i in range(13)]
+
+strats = [{17: 0, 18: 0, 19: 0, 20: 0, 21: 0} for i in range(1, 14)]
+
+def draw_stay_decision(player_hand, upcard, N):
+    # draw card
+    draw_player_hand = player_hand.copy()
+    player_hand_sum = hand_sum_calc(draw_player_hand)
+    stay_hand_sum = player_hand_sum
+    strats[upcard[0][1] - 1][player_hand_sum] = 1
+    draw_payout = 0
+    stay_payout = 0
+    for i in range(N):
+        # print(draw_payout, stay_payout)
+        bust = 0
+        # print('\n\n NEXT')
+        draw_player_hand = player_hand.copy()
+        player_hand_sum = hand_sum_calc(draw_player_hand)
+        while(strats[upcard[0][1] - 1][player_hand_sum] == 1):
+            shown_cards = np.append(draw_player_hand, upcard, axis=0)
+            new_card = drawcard(shown_cards)
+            draw_player_hand = np.append(draw_player_hand, np.array([new_card]), axis=0)
+            player_hand_sum = hand_sum_calc(draw_player_hand)
+            if player_hand_sum > 21:
+                # print('Bust')
+                bust = 1
+                draw_payout += 0
+                break
+        # print(draw_player_hand)    
+        dealer_hand = upcard
+        dealer_hand_sum = hand_sum_calc(dealer_hand)
+        second_card = 0
+        # dealer turn
+        while (dealer_hand_sum < 17):
+            if (second_card == 0):
+                new_card = drawcard(shown_cards)
+                dealer_hand = np.append(dealer_hand, np.array([new_card]), axis=0)
+                dealer_hand_sum = hand_sum_calc(dealer_hand)
+                if (dealer_hand_sum == -1):
+                    draw_payout += 0
+                    stay_payout += 0
+                    continue
+                shown_cards = np.append(shown_cards, np.array([new_card]), axis=0)
+                second_card = 1
+            else:
+                new_card = drawcard(shown_cards)
+                dealer_hand = np.append(dealer_hand, np.array([new_card]), axis=0)
+                dealer_hand_sum = hand_sum_calc(dealer_hand)
+                shown_cards = np.append(shown_cards, np.array([new_card]), axis=0)
+        if (dealer_hand_sum > 21):
+            if (bust == 0):
+                draw_payout += 200
+        elif (dealer_hand_sum > player_hand_sum):
+            draw_payout += 0
+        elif (dealer_hand_sum < player_hand_sum and bust == 0):
+            draw_payout += 200
+        else:
+            if (bust == 0):
+                draw_payout += 100
+        
+        if (dealer_hand_sum > 21):
+            stay_payout += 200
+        elif (dealer_hand_sum > stay_hand_sum):
+            stay_payout += 0
+        elif (dealer_hand_sum < stay_hand_sum):
+            stay_payout += 200
+        else:
+            if (bust == 0):
+                draw_payout += 100
+        # print('DEALER HAND\n', dealer_hand)
+    if (draw_payout > stay_payout):
+        strats[upcard[0][1] - 1][stay_hand_sum] = 1
+    else:
+        strats[upcard[0][1] - 1][stay_hand_sum] = 0
+    return (draw_payout/N, stay_payout/N)
+        
+upcards = [i for i in range(1, 14)]
+# print(strats)
+# for upc_val in upcards:
+#     draw_stay_decision(np.array([[0, 9], [0, 7]]), np.array([[1, upc_val]]), 10000)
+# strat16 = [strats[upc - 1][16] for upc in upcards]
+# plt.plot(upcards, strat16)
+# plt.show()
+# print(strat16)
+sum_dc_strategy = [[k] + [0 for j in range(1, 14)] for k in range(21, 16, -1)]
+for hand_sum in range(16, 11, -1):
+    for upc_val in upcards:
+        draw_stay_decision(np.array([[0, 9], [0, hand_sum - 9]]), np.array([[1, upc_val]]), 10000)
+    sum_dc_strategy.append([hand_sum] + [strats[upc - 1][hand_sum] for upc in upcards])
+print(sum_dc_strategy)
+colormap = cm.get_cmap('tab20', 10)
+colors = [colormap(i) for i in range(10)]
 plt.figure(figsize=(15, 15))
-for i in range(1, 14):
-    plt.plot(cutoffs, np.log(cutoff_res[i-1]), marker='o', linestyle='-', color=colors[i-1], label=f'Dealer Upcard: {i}')
-plt.xlabel('Cutoff Point')
-plt.ylabel('Log of Average Payout for 100 Bet')
-plt.legend(loc='lower left')
-plt.savefig("Figure1.png", dpi=300)
+plt.xticks(range(1, 14, 1))
+plt.plot(upcards, sum_dc_strategy[5][1:], marker='o', linestyle='-', color=colors[4])
+plt.xlabel('Upcards - A: 1, K: 13, Q: 12, J: 11')
+plt.ylabel('Hit/Stand - Hit: 1, Stand: 0')
+plt.savefig("Figure2.png", dpi=300)
+
+
+# cutoff_res = [cutoff_avs_for_upcard(i) for i in range(1, 14)]
+# colormap = cm.get_cmap('tab20', 13)
+# colors = [colormap(i) for i in range(13)]
+# plt.figure(figsize=(15, 15))
+# for i in range(1, 14):
+#     plt.plot(cutoffs, (cutoff_res[i-1]), marker='o', linestyle='-', color=colors[i-1], label=f'Dealer Upcard: {i}')
+# plt.xtick((11, 21, 1))
+# plt.xlabel('Cutoff Point')
+# plt.ylabel('Average Payout for 100 Bet')
+# plt.legend(loc='lower left')
+# plt.savefig("Figure1.png", dpi=300)
 
